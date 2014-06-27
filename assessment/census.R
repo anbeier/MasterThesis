9f97ef6f11dd2e72b59523be08506e6efc8a57ba
Yichi Liu <yichi@wthack.de>
2014-06-27 23:50:24 +0200

    read data

--- assessment/census.R
+++ assessment/census.R
@@ -1,1 +1,4 @@
 census <- read.csv("census.csv", sep = ";")
+colnames <- readLines("census_colnames.txt", encoding = "UTF-8")
+fqs <- readLines("census_fqs_delta0.7_alpha_0.5.txt", encoding = "UTF-8")
+
