4b95ac416dafa08f1daab9381fe12b1c9d73d74d
Yichi Liu <yichi@wthack.de>
2014-06-28 00:13:27 +0200

    extract quasi clique

--- assessment/census.R
+++ assessment/census.R
@@ -1,4 +1,10 @@
 census <- read.csv("census.csv", sep = ";")
 colnames <- readLines("census_colnames.txt", encoding = "UTF-8")
-fqs <- readLines("census_fqs_delta0.7_alpha_0.5.txt", encoding = "UTF-8")
+colnames(census) <- colnames
 
+exprRes <- "census_fqs_delta0.7_alpha_0.5.txt"
+fqs <- readLines(exprRes, encoding = "UTF-8")
+
+# separate the first string line in the fqs into column names
+qs1 <- unlist(strsplit(fqs[1], "--"))
+df1 <- subset(census, select = qs1)
