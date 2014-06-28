c35c307da9267c2a82da1293b9c26f520563f19b
Yichi Liu <yichi@wthack.de>
2014-06-28 20:18:34 +0200

    change "qs" data type

--- assessment/readCensusFqs.R
+++ assessment/readCensusFqs.R
@@ -1,19 +1,16 @@
 # input: file path of fqs.txt
-# output: a character vector of all the final quasi cliques
+# output: a list of character vectors indicating the final quasi cliques
 
 readCensusFqs <- function(fqsFile) {
   
-  census <- read.csv("census.csv", sep = ";")
-  
-  colnames(census) <- readLines("census_colnames.txt", encoding = "UTF-8")
-  
-  fqs <- readLines(fqsFile, encoding = "UTF-8")
-  
+  census <- read.csv("census.csv", sep = ";")  
+  colnames(census) <- readLines("census_colnames.txt", encoding = "UTF-8") 
+  fqs <- readLines(fqsFile, encoding = "UTF-8")  
   qs <- NULL
   
   for(i in 1:length(fqs)) {
     tmp <- unlist(strsplit(fqs[i], "--"))
-    qs <- c(qs, tmp)
+    qs <- append(qs, list(tmp))
   }
   
   return(qs)
