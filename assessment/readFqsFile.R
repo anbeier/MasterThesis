c85a64f7d0b386d119b2a20fc75db92ab2c9b129
Yichi Liu <yichi@wthack.de>
2014-06-28 20:26:11 +0200

    initial commit, to replace readCensusFqs() function

---
+++ assessment/readFqsFile.R
@@ -0,0 +1,17 @@
+# input: file path of fqs.txt
+# output: a list of character vectors indicating the final quasi cliques
+
+readFqsFile <- function(fqsFile) {
+  
+  #  census <- read.csv("census.csv", sep = ";")  
+  #  colnames(census) <- readLines("census_colnames.txt", encoding = "UTF-8") 
+  fqs <- readLines(fqsFile, encoding = "UTF-8")  
+  qs <- NULL
+  
+  for(i in 1:length(fqs)) {
+    tmp <- unlist(strsplit(fqs[i], "--"))
+    qs <- append(qs, list(tmp))
+  }
+  
+  return(qs)
+}
