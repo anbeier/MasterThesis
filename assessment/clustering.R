f5c41769fffbfad256749ac2fa1387cf8f14638e
Yichi Liu <yichi@wthack.de>
2014-07-03 12:13:59 +0200

    add density-based clustering

--- assessment/clustering.R
+++ assessment/clustering.R
@@ -1,23 +1,33 @@
 iris2 <- iris
 iris2$Species <- NULL    ## delete the Species column
-kmeans.result <- kmean(iris2, 3)    ## multi-class
-(kmeans.result)    ## show result
-table(iris$Species, kmeans.result$cluster)    ## compare the result with class labels
-# there are 4 dimensions and we only use the first two of them to plot
-plot(iris2[c("Sepal.Length", "Sepal.Width")], col = kmeans.result$cluster)  
-points(kmeans.result$centers[,c("Sepal.Length", "Sepal.Width")], col = 1:3, pch = 8, cex=2)
+## k-means clustering
+qscl <- dfInt
+qscl$migration_code_change_in_msa.factor <- NULL    ## remove the target variable
+resKmeans <- kmeans(qscl, 9)
 
-# k-means clustering
-df2 <- dfInt
-df2$migration_code_change_in_msa <- NULL
-rsltKmeans <- kmeans(df2, 9)
-(rsltKmeans)
-table(dfInt$migration_code_change_in_msa.factor, rsltKmeans$cluster)
-plot(df2[c("age", "education.factor")], col = rsltKmeans$cluster)
-points(rsltKmeans$centers[, c("age", "education.factor")], col = 1:9, pch = 8, cex = 2)
+# take a look at the clusters
+(resKmeans)
+
+# compare clustering results with class labels
+table(resKmeans$cluster, dfInt$migration_code_change_in_msa.factor)
+
+# plot clusters
+plot(qscl[c("weeks_worked_in_year", "education.factor")], col = resKmeans$cluster)
+points(resKmeans$centers[, c("weeks_worked_in_year", "education.factor")], col = 1:9, pch = 20, cex = 2)
 
 
-# hierarchical clustering
+## density-based clustering
+install.packages("fpc")
+library(fpc)
+qscl <- dfInt
+qscl$migration_code_change_in_msa.factor <- NULL
+ds <- dbscan(qscl, eps=0.42, MinPts=5)
+table(ds$cluster, dfInt$migration_code_change_in_msa.factor)
+
+plot(ds, qscl)
+
+
+## hierarchical clustering
 # map age range to 19 classes
 distances <- dist(dfInt, method = "euclidean")
 migrationCluster <- hclust(distances, method = "ward.D") 
