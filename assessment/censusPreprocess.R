99305da57761bfb9586eb6932a599e9eba5db8e1
Yichi Liu <yichi@wthack.de>
2014-07-04 13:01:43 +0200

    initial commit

---
+++ assessment/censusPreprocess.R
@@ -0,0 +1,37 @@
+# read quasi cliques
+# input: fqs.txt file path
+# output: a list of character vectors indicating the quasi cliques
+readFqsFile <- function(fqsFile) {
+  
+  fqs <- readLines(fqsFile, encoding = "UTF-8")  
+  qs <- NULL
+  
+  for(i in 1:length(fqs)) {
+    tmp <- unlist(strsplit(fqs[i], "--"))
+    qs <- append(qs, list(tmp))
+  }
+  
+  return(qs)
+}
+
+
+# choose a few samples from a clique w.r.t a specific target value
+# input: a quasi clique, a target value
+# output: a sensible amount of samples for training
+split <- NULL
+samples <- NULL
+trainingSamples <- function(qs, targetval) {
+  
+  samples <- qs[sample(nrow(qs), replace = FALSE, size = 0.15 * nrow(qs))]  
+  ## install.packages("caTools")
+  library(caTools)
+  split <- sample.split(samples[, targetval], SplitRatio = 0.6)
+  training <- subset(samples, split == TRUE)
+  return(training)
+}
+
+# output: a sensible amount of samples for testing
+testingSamples <- function() {
+  
+  return(subset(samples, split == FALSE))
+}
