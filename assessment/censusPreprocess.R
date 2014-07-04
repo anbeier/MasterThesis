5dd6d8768bb4657099aa63bcdb5b9dad759e88ed
Yichi Liu <yichi@wthack.de>
2014-07-04 13:31:16 +0200

    define variables split & samples using <<- operator; add reviseColnames() & makeAgeInterval() functions

--- assessment/censusPreprocess.R
+++ assessment/censusPreprocess.R
@@ -18,14 +18,13 @@ readFqsFile <- function(fqsFile) {
 # choose a few samples from a clique w.r.t a specific target value
 # input: a quasi clique, a target value
 # output: a sensible amount of samples for training
-split <- NULL
-samples <- NULL
 trainingSamples <- function(qs, targetval) {
   
-  samples <- qs[sample(nrow(qs), replace = FALSE, size = 0.15 * nrow(qs))]  
+  samples <<- qs[sample(nrow(qs), replace = FALSE, size = 0.15 * nrow(qs))]  
+  
   ## install.packages("caTools")
   library(caTools)
-  split <- sample.split(samples[, targetval], SplitRatio = 0.6)
+  split <<- sample.split(samples[, targetval], SplitRatio = 0.6)
   training <- subset(samples, split == TRUE)
   return(training)
 }
@@ -34,4 +33,27 @@ trainingSamples <- function(qs, targetval) {
 testingSamples <- function() {
   
   return(subset(samples, split == FALSE))
+}
+
+
+# replace invalid symbols in column names with "_"
+# input: a quasi clique
+# output: a quasi clique with all valid column names
+reviseColnames <- function(qs) {
+  
+  for(i in 1:ncol(qs)) {
+    colnames(qs)[i] <- gsub(" ", "_", colnames(qs)[i], fixed = TRUE)
+    colnames(qs)[i] <- gsub("-", "_", colnames(qs)[i], fixed = TRUE)
+  }
+  
+  return(qs)
+}
+
+
+# categorize age into group
+makeAgeInterval <- function(qs) {
+  
+  qs$age.group <- findInterval(qs$age, seq(0, 90, 10))
+  qs$age <- NULL
+  return(qs)
 }
