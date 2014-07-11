f184f56f88396868f8f5a06aa0c10fe4f8bda8eb
Yichi Liu <yichi@wthack.de>
2014-07-11 18:00:25 +0200

    enable main() function to write csv files of samples

--- assessment/census.R
+++ assessment/census.R
@@ -1,41 +1,25 @@
-csvFile = "census.csv"
-colnameFile = "census_colnames.txt"
-specialCols = c(3, 4, 36, 38, 40)
-fqsFile = "census_fqs_delta0.7_alpha0.5.txt"
-alpha = 0.5
+# specialCols = c(3, 4, 36, 38, 40)
 
-main <- function(csvFile, colnameFile, alpha, fqsFile) {
+# returns a quasi clique by giving the its index
+main <- function(index, alpha = 0.5, csvFile = "census.csv", 
+                 colnameFile = "census_colnames.txt", fqsFile = "census_fqs_delta0.7_alpha0.5.txt") {
   
   census <- getDataset(csvFile, colnameFile, alpha)
   cliques <- readingQuasiCliques(census, fqsFile)
   
   # here is a loop modelling for each clique and for each column in a clique
   # loop 1: take the 1st clique for example
-  index <- 1
   qs <- getOneClique(census, cliques, index)
   
   # here is the 2nd loop which is for each column in the clique
   # loop 2: take one column for example
-  target = "migration_code_change_in_msa"
-  df <- takeSamples(qs, target)
+  # target = "migration_code_change_in_msa"
+  # df <- takeSamples(qs, target)
   
-  res <- getTrainingResultsOfOneClique(qs)
+  # here is to train a SVM model regarding to a specific target value in a clique
+  # res <- getTrainingResultsOfOneClique(qs)
   
-   
+  # get a list of data frames regarding to a specific target value, respectively
+  ls <- makeSamplesForEachTarget(qs, cliques[[index]])
+  writeCSVForEachSamples(ls)
 }
-
-
-
-
-
-# preprocess: categorize age into group and make it factor
-df <- makeAgeIntervalFactor(df)    ## not testet yet
-
-# preprocess: categorize worked weeks into group
-df <- makeWeekIntervalFactor(df)    ## not testet yet
-
-# preprocess: convert education factor to integers by hand
-qs <- makeEducationCat(qs)
-
-# proprocess: convert factors to integers using mapLevels()
-qs <- convertCategories(qs)
