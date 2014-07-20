f65e2c13db93dda0c7066dc154f23e7c513a600c
Yichi Liu <yichi@wthack.de>
2014-07-20 22:10:24 +0200

    change makeFilePath() to getFileNames() getting a list of file names in the given directory; add getFileContents() function merging all experiment results to one data frame

--- assessment/censusPostAnalysis.R
+++ assessment/censusPostAnalysis.R
@@ -1,21 +1,32 @@
 getDirectory <- function(delta, alpha) {
-  prefix <- paste(paste('delta', delta, sep = ''), paste('alpha', alpha, sep = ''), sep = '-')
+  prefix <- paste(paste('delta', delta, sep = ''), paste('alpha', alpha, sep = ''), sep = '_')
   foldername <- paste('census', prefix, sep = '_')
   return(foldername)
 }
 
-makeFilePath <- function(index, delta, alpha) {
-  prefix <- paste(paste('delta', delta, sep = ''), paste('alpha', alpha, sep = ''), sep = '-')
-  foldername <- paste('census', prefix, sep = '_')
-  filename <- paste(prefix, paste('qs', index, sep = ''), sep = '-')
-  filename <- paste(filename, 'RData', sep = '.')
-  filepath <- paste(foldername, filename, sep = '/')
-  return(filepath)
+getFileNames <- function(directory) {
+  filenames <- 
+  return(filenames)
+}
+
+getFileContents <- function(filenames) {
+  dfErrorOfEachColumn <- NULL
+  dfCriticalErrorOfEachClique <- NULL
+  for(fn in filenames) {
+    load(fn)    # Will have a rslt value in the environment after loading a file
+    index <- rep(rslt$clique_index, nrow(rslt$details))
+    df <- data.frame(clique_index = index, rslt$details)
+    dfErrorOfEachColumn <- rbind(dfErrorOfEachColumn, df)
+    df <- data.frame(clique_index = rslt$clique_index, min_error = rslt$min_error, 
+                     error_threshold = rslt$threshold, avg_error = rslt$avg_error)
+    dfCriticalErrorOfEachClique <- rbind(dfCriticalErrorOfEachClique, df)
+  }
+  list(ErrorOfEachColumn = dfErrorOfEachColumn, 
+       CriticalErrorOfEachClique = dfCriticalErrorOfEachClique)
 }
 
 readExperimentResults <- function(delta, alpha) {
   dir <- getDirectory(delta, alpha)
-  
-  fileList <- list.files(path = file)
-  # how many files in this file path?
+  filenames <- list.files(dir, full.names = TRUE)  # only 98 files, it should be 102
+  results <- getFileContents(filenames)
 }
