8cbafdb16a556af95404f6a52fabe2e290614676
Yichi Liu <yichi@wthack.de>
2014-08-04 21:44:43 +0200

    update

--- assessment/actPreprocess.R
+++ assessment/actPreprocess.R
@@ -17,7 +17,23 @@ readingQuasiCliques <- function(fqsFile) {
 giveColumnName <- function(aClique) {
   res <- unlist(lapply(as.list(aClique), 
                       function(x) {
-                        paste('V', x, sep = '')
+                        paste('V', as.numeric(x) + 1, sep = '')  ## indices in fqs file starting at 0
                       }))
   return(res)
+}
+
+getOneClique <- function(dataset, cliqueGroups, cliqueIndex) {
+  res <- subset(dataset, select = cliqueGroups[[cliqueIndex]])
+  return(res)
+}
+
+splitData <- function(data, seed = 100) {
+  if (!is.null(seed)) {
+    set.seed(seed)
+  }
+  sampleSize <- floor(0.65 * nrow(data))  ## 65% of the dataset size
+  trainIndex <- sample(seq_len(nrow(data)), size = sampleSize)
+  trainset <- data[trainIndex, ]
+  testset <- data[-trainIndex, ]
+  list(trainset=trainset,testset=testset)
 }
