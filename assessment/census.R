f1545000cb2731311998b6008c4d10122cf04a3c
Yichi Liu <yichi@wthack.de>
2014-08-30 13:46:07 +0200

    added tryDifferentTechniques() and put all machine learning technqiues into analyticalTechniques.R

--- assessment/census.R
+++ assessment/census.R
@@ -2,9 +2,10 @@ library(parallel)
 library(e1071)
 library(caTools)
 library(arules)
+
 source('censusPreprocess.R')
-source('censusSVM.R')
-source('censusAssociationRules.R')
+source('analyticalTechniques.R')
+source('censusPostAnalysis.R')
 source('log.R')
 
 csvfp = 'census.csv'
@@ -13,6 +14,22 @@ fqsfp = 'census_fqs_delta0.7_alpha0.5.txt'
 d = 0.7
 a = 0.5
 
+tryDifferentTechniques <- function(csvFile = csvfp, colnameFile = colnamefp,
+                              fqsFile = fqsfp, delta = d, alpha = a) {
+  census <- getCensusData(csvFile, colnameFile)
+  cliqueGroup <- readingQuasiCliques(fqsFile) 
+  # Take the 1st quasi clique as sample data
+  sampleClique <- getOneClique(census, cliqueGroup, 1) 
+  fileIndicator <- makeFileIndicator(delta, alpha, 1)
+  
+  result.svm <- loopTrainSVMForOneClique(sampleClique, 1, fileIndicator)
+  result.bayes <- loopTrainNaiveBayesForOneClique(sampleClique, 1, fileIndicator)
+  
+  mcc.svm <- returnMCCTable(result.svm)
+  mcc.bayes <- returnMCCTable(result.bayes)
+  mcc <- merge(mcc.svm, mcc.bayes, by = 'Column')
+  return(mcc)
+}
 
 main <- function(cliqueIndex, delta = d, alpha = a, 
                  csvFile = csvfp, colnameFile = colnamefp, fqsFile = fqsfp) {
@@ -24,17 +41,17 @@ main <- function(cliqueIndex, delta = d, alpha = a,
   # Save result of each clique in a 'rdata' file
   fileIndicator <- makeFileIndicator(delta, alpha, cliqueIndex)
   loopTrainSVMForOneClique(clique, cliqueIndex, fileIndicator)
-  loopAssociationRules(clique, cliqueIndex, delta, alpha)
+  loopTrainNaiveBayesForOneClique(clique, cliqueIndex, fileIndicator)
   rm(list = ls())  ## clear workspace
 }
 
 worker <- function(input) {
-  #log(paste("processing", input$index))
+  log(paste("processing", input$index))
   tryCatch({
-    loopTrainSVMForOneClique(input$clique, input$index, input$filename)
-    #loopAssociationRules(input$clique, input$index, input$delta, input$alpha)
+    #loopTrainSVMForOneClique(input$clique, input$index, input$filename)
+    loopTrainNaiveBayesForOneClique(input$clique, input$index, input$filename)
   }, error=function(e) {
-    #log(paste("error processing", input$index, e))
+    log(paste("error processing", input$index, e))
     e
   })
 }
@@ -46,7 +63,7 @@ parallelMain <- function(indexStart, indexEnd, delta = d, alpha = a,
   cliques <- readingQuasiCliques(fqsFile)
   
   inputs <- lapply(seq(indexStart, indexEnd), function(i) {
-    #log(paste("prepare input for", i))
+    log(paste("prepare input for", i))
     
     list(index = i, 
          clique = getOneClique(census, cliques, i), 
