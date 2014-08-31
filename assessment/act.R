c06b8bbc97e774a34756482e3f3142add953fe38
Yichi Liu <yichi@wthack.de>
2014-08-31 23:31:18 +0200

    added tryDifferentTechniques()

--- assessment/act.R
+++ assessment/act.R
@@ -1,15 +1,41 @@
+library(monmlp)
+library(caret)
+
 source('actPreprocess.R')
-source('actLinearRegression.R')
+source('analyticalTechniques.R')
 
-csvFile = 'act.csv'
-fqsFile = 'act_tutor.txt'
+csvfp = 'act.csv'
+fqsfp = 'act_tutor.txt'
+d = 0.7
+a = 0.5
 
-main <- function(csvfp = csvFile, fqsfp = fqsFile, delta, alpha) {
-  data <- readingActData(csvFile)
-  cliqueGroups <- readingQuasiCliques(fqsFile)
+tryDifferentTechniques <- function(csvFile = csvfp, fqsFile = fqsfp, delta = d, alpha = a) {
+  act <- readActData(csvFile)
+  cliqueGroup <- readQuasiCliques(fqsFile)
+  sampleClique <- getOneClique(act, cliqueGroup, 1) 
+  fileIndicator <- makeFileIndicator(delta, alpha, 1)
+  
+  result.lm <- loopTrainLinearRegressionForOneClique(sampleClique, 1, fileIndicator)
+  result.percp <- loopTrainMultilayerPerceptron(sampleClique, 1, fileIndicator)
+  result.cart <- loopTrainClassificationRegressionTree(sampleClique, 1, fileIndicator)
+  
+  rmsd.lm <- returnRMSDTable(result.lm$result)
+  rmsd.percp <- returnRMSDTable(result.percp$result)
+  rmsd.cart <- returnRMSDTable(result.cart$result)
+  
+  data.frame(target = rmsd.lm$target,
+             RMSD_lm = rmsd.lm$RMSD,
+             RMSD_perceptron = rmsd.percp$RMSD,
+             RMSD_cart = rmsd.cart$RMSD)
+}
+
+
+main <- function(cliqueIndex, delta = d, alpha = a, csvFile = csvfp, fqsFile = fqsfp) {
+  data <- readActData(csvFile)
+  cliqueGroups <- readQuasiCliques(fqsFile)
   for(i in 1:length(cliqueGroups)) {
     clique <- getOneClique(data, cliqueGroups, i)
-    loopTrainingTestingLM(clique, i, delta, alpha)
+    loopTrainLinearRegressionForOneClique(clique, i, delta, alpha)
   }
-  rm(list = ls())  ## remove all user-defined objects
+  rm(list = ls())  ## clear working space
 }
