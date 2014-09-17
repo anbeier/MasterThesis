246e8be58969cabc231a608d58fcf7fe717f64c5
Yichi Liu <yichi@wthack.de>
2014-09-18 01:30:16 +0200

    added parallelMain, worker functions

--- assessment/act.R
+++ assessment/act.R
@@ -1,13 +1,17 @@
 library(monmlp)
 library(caret)
+library(parallel)
 
 source('actPreprocess.R')
 source('analyticalTechniques.R')
+source('actPostAnalysis.R')
+source('log.R')
 
 csvfp = 'act.csv'
 fqsfp = 'act_tutor.txt'
 d = 0.7
 a = 0.5
+
 
 tryDifferentTechniques <- function(csvFile = csvfp, fqsFile = fqsfp, delta = d, alpha = a) {
   act <- readActData(csvFile)
@@ -38,4 +42,33 @@ main <- function(cliqueIndex, delta = d, alpha = a, csvFile = csvfp, fqsFile = f
     loopTrainLinearRegressionForOneClique(clique, i, delta, alpha)
   }
   rm(list = ls())  ## clear working space
+}
+
+worker <- function(input) {
+  log(paste("processing", input$index))
+  tryCatch({
+    #loopTrainSVMForOneClique(input$clique, input$index, input$filename)
+    loopTrainClassificationRegressionTree(input$clique, input$index, input$filename)
+  }, error=function(e) {
+    log(paste("error processing", input$index, e))
+    e
+  })
+}
+
+parallelMain <- function(indexStart, indexEnd, delta = d, alpha = a, csvFile = csvfp, fqsFile = fqsfp) {
+  act <- readActData(csvFile)
+  cliqueGroup <- readQuasiCliques(fqsFile)
+  
+  inputs <- lapply(seq(indexStart, indexEnd), function(i) {
+    log(paste("prepare input for", i))
+    
+    list(index = i, 
+         clique = getOneClique(act, cliqueGroup, i) , 
+         filename = makeFileIndicator(delta, alpha, i))
+  })
+  
+  cores <- 2 # be friendly and dont use all cores
+  
+  res <- mclapply(inputs, worker, mc.cores = cores)
+  return(res)
 }
