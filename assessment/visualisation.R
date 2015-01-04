4be317f1ac3424fe3f05dee5b692e2f4e3f01574
Yichi Liu <yichi@wthack.de>
2015-01-04 18:58:14 +0100

    modified plot_TargetCols()

--- assessment/visualisation.R
+++ assessment/visualisation.R
@@ -136,6 +136,7 @@ countColsByClique <- function(d) {
 }
 
 avgMCCByClique <- function(d) {
+  d$mcc = as.numeric(as.character(d$mcc))
   aggregate(d$mcc, by=list(d$index), FUN=mean)
 }
 
@@ -165,45 +166,63 @@ findQSHavingSameNCols <- function(df) {
   agg$Group.1[agg$x == TRUE]
 }
 
-plot_TargetCols <- function(delta) {
-  x = 'census_delta'
-  fn.bayes = paste(paste(x, delta, sep=''), '_alpha0.5/bayes/mcc/results.csv', sep= '')
-  fn.svm = paste(paste(x, delta, sep=''), '_alpha0.5/svm/mcc/results.csv', sep= '')
+readMCCResults <- function(method, delta) {
+  prefix = paste(method, "delta", sep='_')
+  fn.bayes = paste(paste(prefix, delta, sep=''), '_alpha0.5/bayes/mcc/results.csv', sep= '')
+  fn.svm = paste(paste(prefix, delta, sep=''), '_alpha0.5/svm/mcc/results.csv', sep= '')
   
   # read result.csv
   b = read.csv(fn.bayes, header=T, sep=',')
   s = read.csv(fn.svm, header=T, sep=',')
   
-  b.agg = merge(countColsByClique(b), avgMCCByClique(b), by='Group.1')
+  list(bayes=b, svm=s)
+}
+
+adjustCliquesFromBothClassifiers <- function(d1, d2) {
+  b.agg = addNotInCols(d2, d1)
+  s.agg = addNotInCols(d1, d2)
+  rbind(b.agg, s.agg)
+}
+
+modifyDataTypeForPlot <- function(dat) {
+  dat$qs = factor(paste('QS', dat$index, sep='_'), levels=makeLevels(unique(dat$index)))
+  dat$mcc = round(as.numeric(dat$mcc), 4)
+  dat$ncol = as.integer(dat$ncol)
+  return(dat)
+}
+
+determineYMin <- function(x) {
+  if(is.na(match(0, as.integer(x)))) {
+    return(1)
+  } else {
+    return(0)
+  }
+}
+
+plot_MCCResults <- function(method, delta, yminMCC=0.9) {
+  ls = readMCCResults(method, delta)
+  
+  b.agg = merge(countColsByClique(ls$bayes), avgMCCByClique(ls$bayes), by='Group.1')
   names(b.agg) = c('index', 'ncol', 'mcc')
   b.agg$method = 'bayes'
-  s.agg = merge(countColsByClique(s), avgMCCByClique(s), by='Group.1')
+  s.agg = merge(countColsByClique(ls$svm), avgMCCByClique(ls$svm), by='Group.1')
   names(s.agg) = c('index', 'ncol', 'mcc')
   s.agg$method = 'svm'
   
-  b.agg = addNotInCols(s.agg, b.agg)
-  s.agg = addNotInCols(b.agg, s.agg)
-  
-  dat = rbind(b.agg, s.agg)
-  dat$qs = factor(paste('QS', dat$index, sep='_'), levels=makeLevels(unique(dat$index)))
-  dat$mcc = round(as.numeric(dat$mcc), 4)
+  dat = adjustCliquesFromBothClassifiers(b.agg, s.agg)
+  dat = modifyDataTypeForPlot(dat)
   
   # determine y axis for number of target columns
-  ymin = NULL
-  if(is.na(match(0, dat$ncol))) {
-    ymin = 0
-  } else {
-    ymin = 1
-  }
+  #ymin = determineYMin(dat$ncol)
   
   image = ggplot(dat, aes(x=qs, y=ncol, fill = method)) +
     geom_bar(stat="identity", position="dodge") +
-    scale_fill_manual(name="Classifier", values=c('lightskyblue4','palevioletred')) +
-    coord_cartesian(ylim=seq(ymin, as.integer(max(dat$ncol)), by=1)) +
-    theme(text = element_text(size=18), 
+    scale_fill_manual(name="Classifier", values=c('midnightblue','palevioletred')) +
+    coord_cartesian(ylim=seq(0, as.integer(max(dat$ncol)), by=1)) +
+    theme(text = element_text(size=16), 
           legend.position='none',
           legend.title=element_blank(),
-          axis.text.x  = element_text(angle=45, vjust=0.5, size=15)) +
+          axis.text.x  = element_text(angle=45, vjust=0.5, size=13)) +
     xlab('Discovered Qualified Quasi-cliques') +
     ylab('Number of Identified Target Column')
   
@@ -215,12 +234,70 @@ plot_TargetCols <- function(delta) {
   
   image = ggplot(dat, aes(x=qs, y=mcc, fill = method)) +
     geom_bar(stat="identity", position="dodge") +
-    scale_fill_manual(name="Classifier", values=c('lightskyblue4','palevioletred')) +
-    coord_cartesian(ylim=seq(0.9, 1, by=0.1)) +
-    theme(text = element_text(size=18), 
+    scale_fill_manual(name="Classifier", values=c('midnightblue','palevioletred')) +
+    coord_cartesian(ylim=seq(yminMCC, 1, by=0.1)) +
+    theme(text = element_text(size=16), 
           legend.position='none',
           legend.title=element_blank(),
-          axis.text.x  = element_text(angle=45, vjust=0.5, size=15)) +
+          axis.text.x  = element_text(angle=45, vjust=0.5, size=13)) +
+    xlab('Discovered Qualified Quasi-cliques') +
+    ylab('Mean MCC Value of Target Columns')
+  
+  ggsave(file="target_mcc.svg", plot=last_plot(), width=6, height=5)
+}
+
+plot_TargetCols_census07 <- function(delta) {
+  ls = readMCCResults("census", delta)
+  
+  b.agg = merge(countColsByClique(ls$bayes), avgMCCByClique(ls$bayes), by='Group.1')
+  names(b.agg) = c('index', 'ncol', 'mcc')
+  b.agg$method = 'bayes'
+  s.agg = merge(countColsByClique(ls$svm), avgMCCByClique(ls$svm), by='Group.1')
+  names(s.agg) = c('index', 'ncol', 'mcc')
+  s.agg$method = 'svm'
+  
+  dat = adjustCliquesFromBothClassifiers(b.agg, s.agg)
+  dat = modifyDataTypeForPlot(dat)
+  
+  # separate dat into 2 subsets
+  dat = dat[order(as.integer(dat$index)),]
+  dat1 = head(dat, 64)
+  dat2 = dat[!dat$index %in% dat1$index,]
+  
+  image1 = ggplot(dat1, aes(x=qs, y=ncol, fill = method)) +
+    geom_bar(stat="identity", position="dodge") +
+    scale_fill_manual(name="Classifier", values=c('midnightblue','palevioletred')) +
+    coord_cartesian(ylim=seq(0, as.integer(max(dat$ncol)), by=1)) +
+    theme(text = element_text(size=16), 
+          legend.position='none',
+          legend.title=element_blank(),
+          axis.text.x  = element_text(angle=90, vjust=0.5, size=12)) +
+    xlab('Discovered Qualified Quasi-cliques') +
+    ylab('Number of Identified Target Column')
+  
+  image2 = ggplot(dat2, aes(x=qs, y=ncol, fill = method)) +
+    geom_bar(stat="identity", position="dodge") +
+    scale_fill_manual(name="Classifier", values=c('midnightblue','palevioletred')) +
+    coord_cartesian(ylim=seq(0, as.integer(max(dat$ncol)), by=1)) +
+    theme(text = element_text(size=16), 
+          legend.position='none',
+          legend.title=element_blank(),
+          axis.text.x  = element_text(angle=90, vjust=0.5, size=12)) +
+    xlab('Discovered Qualified Quasi-cliques') +
+    ylab('Number of Identified Target Column')
+  
+  image = grid.arrange(image1, image2, ncol=2)
+  
+  dat = subset(dat, dat$index %in% findQSHavingSameNCols(dat))
+  
+  image = ggplot(dat, aes(x=qs, y=mcc, fill = method)) +
+    geom_bar(stat="identity", position="dodge") +
+    scale_fill_manual(name="Classifier", values=c('midnightblue','palevioletred')) +
+    coord_cartesian(ylim=seq(0.6, 1, by=0.1)) +
+    theme(text = element_text(size=16), 
+          legend.position='none',
+          legend.title=element_blank(),
+          axis.text.x  = element_text(angle=90, vjust=0.5, size=13)) +
     xlab('Discovered Qualified Quasi-cliques') +
     ylab('Mean MCC Value of Target Columns')
   
